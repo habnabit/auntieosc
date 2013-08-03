604ce6a40fa8ac7873ef7ebd4472d5b07e88cf33
Aaron Gallagher <habnabit@gmail.com>
2013-08-02 22:49:23 -0700

    Initial commit.

---
+++ auntieosc.py
@@ -0,0 +1,103 @@
+from __future__ import division
+
+import argparse
+import datetime
+
+from dateutil.parser import parse as parse_datetime
+import parsley
+import yaml
+
+
+irssi_grammar_source = """
+
+nl = '\n'
+skip_to_end = <(~nl anything)*>:content nl -> content
+word = <(~' ' ~nl anything)+>
+timestamp = <digit{2}>:hour ':' <digit{2}>:minute -> datetime.time(int(hour), int(minute))
+
+log_opened = '--- Log opened ' skip_to_end:when -> parse_datetime(when)
+log_closed = '--- Log closed ' skip_to_end
+
+presence_action_word = ('joined' | 'left' | 'quit')
+presence_action = '(-) ' word:nick ' ' word ' has ' presence_action_word:action ' ' skip_to_end -> (action, nick)
+privmsg = '<' anything <(~'>' anything)+>:nick '> ' skip_to_end -> ('msg', nick)
+emote = ' * ' word:nick ' ' skip_to_end -> ('msg', nick)
+nick_change = '(-) ' word:oldnick ' is now known as ' word:newnick skip_to_end -> ('nick', (oldnick, newnick))
+cruft = '(-) ' skip_to_end:cruft -> ('cruft', cruft)
+
+line = timestamp:when ' ' (presence_action | privmsg | emote | nick_change | cruft):(action, arg) -> (action, when, arg)
+
+document = log_opened:when line*:lines log_closed? -> (when, lines)
+
+"""
+
+irssi_parser = parsley.makeGrammar(
+    irssi_grammar_source, dict(parse_datetime=parse_datetime, datetime=datetime))
+
+
+class Auntieosc(object):
+    lines_buckets = [0, 1, 10, 100]
+
+    def __init__(self):
+        self.users = {}
+
+    def main(self, argv=()):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('infiles', type=argparse.FileType('rb'), nargs='+')
+        parser.add_argument('-w', '--write', type=argparse.FileType('wb'))
+        args = parser.parse_args(argv)
+
+        for infile in args.infiles:
+            with infile:
+                contents = infile.read()
+            base_datetime, events = irssi_parser(contents).document()
+            for action, when, arg in events:
+                when = base_datetime.combine(base_datetime, when)
+                action_method = getattr(self, 'action_' + action, None)
+                if action_method is not None:
+                    action_method(when, arg)
+
+        if args.write:
+            with args.write as outfile:
+                yaml.safe_dump(self.users, outfile, default_flow_style=False)
+
+    def user(self, nick):
+        return self.users.setdefault(nick, {})
+
+    def popuser(self, nick):
+        return self.users.pop(nick, {})
+
+    def action_joined(self, when, nick):
+        user = self.user(nick)
+        user['joined-at'] = when
+        user['lines'] = 0
+
+    def action_msg(self, when, nick):
+        user = self.user(nick)
+        user.setdefault('joined-at', when)
+        user['lines'] = user.get('lines', 0) + 1
+        user['last-talked-at'] = when
+
+    def action_quit(self, when, nick):
+        user = self.user(nick)
+        if not user.get('joined-at'):
+            return
+        time_spent = (when - user['joined-at']).total_seconds()
+        lines = user.pop('lines', 0)
+        for bucket in self.lines_buckets:
+            if lines <= bucket:
+                break
+        else:
+            bucket = None
+        visits = user.setdefault('visits', {})
+        visits[bucket] = visits.get(bucket, 0) + time_spent
+
+    action_left = action_quit
+
+    def action_nick(self, when, (oldnick, newnick)):
+        self.users[newnick] = self.popuser(oldnick)
+
+
+if __name__ == '__main__':
+    import sys
+    Auntieosc().main(sys.argv[1:])
